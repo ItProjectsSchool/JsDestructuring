47625cb52092c958c589ab9a2626a155159a8149
ItProjectsSchool <65037900+ItProjectsSchool@users.noreply.github.com>
2020-05-08 20:53:07 +0300

    Create array_destructuring.js

---
+++ array_destructuring.js
@@ -0,0 +1,56 @@
+let numbers = [1,2,3,4,5];
+
+//Example 1
+//let a = numbers[0]
+//let b = numbers[1]
+//console.log(a,b)
+
+///let [a,b] = numbers
+//console.log(a,b)
+
+//Example 2
+//let [a,b,c] = numbers
+//console.log(a,b,c)
+
+//Example 3
+//let [a,,b]=numbers
+//console.log(a,b)
+
+//Example 4
+//let [a, b, ...rest]=numbers
+//console.log(rest)
+//let [...arr1, ...arr2] = numbers
+
+//Example 5
+//let numbers2 = [1, [2,[3]]]
+//let [a,[b,[c]]] = numbers2
+//console.log(a,b,c)
+
+//Example 6
+//let [a = 1]=[]
+//let [,,,,,,,,b = 2]=numbers
+//console.log(a,b)
+
+//Example 7
+//let array1=[...numbers]
+//console.log(array1 === numbers)
+//console.log(array1)
+
+//let [...array2]=numbers
+//console.log(array2 === numbers)
+//console.log(array2)
+
+//Example 8
+//let array = [...numbers, ...numbers]
+//console.log(array)
+
+//Example 9
+//function firstAndLastItem(someArray){
+//  return [someArray[0], someArray[someArray.length-1]]
+//}
+//let [first, last] =firstAndLastItem(numbers)
+//console.log(first, last)
+
+
+
+
